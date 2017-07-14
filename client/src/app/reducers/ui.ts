e301bc074d40a2987e76d25d3d687eaaf40ef32d
Grayson Gilmore <gilmoreg@live.com>
2017-07-14 14:57:48 -0500

    Add ui reducer functions

--- client/src/app/reducers/ui.ts
+++ client/src/app/reducers/ui.ts
@@ -1,20 +1,28 @@
 import { Action } from '@ngrx/store';
 
+export const CHANGE_TAB = 'CHANGE_TAB';
 export const LOGIN = 'LOGIN';
 export const LOGOUT = 'LOGOUT';
+export const SET_ACTIVE_SEARCH = 'SET_ACTIVE_SEARCH';
+export const SET_FORMAT = 'SET_FORMAT';
+export const SET_USER = 'SET_USER';
 
 const initialState = {
-  format:<string> '',
-  user:<string> '',
   activeTab:<number> 0,
-  currentSearch:<string> '',
+  activeSearch:<string> '',
+  format:<string> '',
   loggedIn:<boolean> false,
+  user:<string> '',
 }
 
 export function uiReducer(state:object = initialState, action:Action) {
   switch (action.type) {
+    case 'CHANGE_TAB': return Object.assign({}, state, { activeTab: action.payload.tab });
     case 'LOGIN': return Object.assign({}, state, { loggedIn: true });
     case 'LOGOUT': return Object.assign({}, state, { loggedIn: false });
+    case 'SET_ACTIVE_SEARCH': return Object.assign({}, state, { currentSearch: action.payload.search });
+    case 'SET_FORMAT': return Object.assign({}, state, { format: action.payload.format });
+    case 'SET_USER': return Object.assign({}, state, { user: action.payload.user });
     default: return state;
   }
 }
