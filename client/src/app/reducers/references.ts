09d694328d8e3fbea8a3187077ff6bbe310536b8
Grayson Gilmore <gilmoreg@live.com>
2017-07-14 15:13:28 -0500

    Add references reducer functions

--- client/src/app/reducers/references.ts
+++ client/src/app/reducers/references.ts
@@ -1,10 +1,25 @@
 import { Action } from '@ngrx/store';
 
 export const ADD_REFERENCE = 'ADD_REFERENCE';
+export const DELETE_REFERENCE = 'DELETE_REFERENCE';
+export const SYNC_REFERENCES = 'SYNC_REFERENCES';
+export const UPDATE_REFERENCE = 'UPDATE_REFERENCE';
 
 export function referencesReducer(state:Array<object> = [], action:Action) {
   switch (action.type) {
     case 'ADD_REFERENCE': return [...state, action.payload.reference];
+    case 'DELETE_REFERENCE': {
+      return state.filter((ref: any) => ref.id !== action.payload.id);
+    }
+    case 'SYNC_REFERENCES': return action.payload.references;
+    case 'UPDATE_REFERENCE': {
+      return state.map((ref:any) => {
+        if (ref.id === action.payload.id) {
+          return action.payload.data;
+        }
+        return ref;
+      });
+    }
     default: return state;
   }
 }
