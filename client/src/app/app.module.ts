35e5c5a2e73dea9f4439e042b720ba76cf923184
Grayson Gilmore <gilmoreg@live.com>
2017-07-14 13:24:32 -0500

    Style and display navbar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingComponent
+    LandingComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
