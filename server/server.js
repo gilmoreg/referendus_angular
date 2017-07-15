fc8fa1898c1e9f37540e340775a4d21d3a01fc32
Grayson Gilmore <gilmoreg@live.com>
2017-07-14 19:05:20 -0500

    Fix static file serving

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const compression = require('compression');
 const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const { PORT, DATABASE_URL } = require('./config');
 const db = mongoose.createConnection(DATABASE_URL, { useMongoClient: true });
@@ -33,7 +34,7 @@ app.use(cors({
 app.use(compression({ level: 9, threshold: 0 }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('../client/dist'));
+app.use('/', express.static(path.join(__dirname, '../client/dist')));
 // app.use(router);
 
 // Log all requests
@@ -42,11 +43,14 @@ app.use((req, res, next) => {
   next();
 });
 
+/*
+Re-enable once router is implemented
 // Log errors
 app.use((err, req, res) => {
   console.error(err);
   res.status(500).json({ error: 'Something went wrong' }).end();
 });
+*/
 
 const runServer = (databaseUrl = DATABASE_URL, port = PORT) =>
   new Promise((resolve, reject) => {
