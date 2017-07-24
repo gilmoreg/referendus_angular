8c17edcdf28aa3ff3690db3a5ced989f836644ea
Grayson Gilmore <gilmoreg@live.com>
2017-07-23 18:40:05 -0600

    Sync references on format change

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -11,15 +11,53 @@ export class AppComponent implements OnInit {
   title: 'Referendus';
   loggedIn$: Observable<boolean>;
   format$: Observable<string>;
+  apaFormat$: Observable<boolean>;
+  chicagoFormat$: Observable<boolean>;
+  mlaFormat$: Observable<boolean>;
+  format: string;
+  isLoggedIn: boolean;
 
   constructor(private store: Store<any>) {}
 
+  syncReferences() {
+    console.log('syncReferences', this.format);
+    if (this.isLoggedIn && this.format) this.store.dispatch({ type: 'SYNC', payload: this.format });
+  }
+
   ngOnInit() {
     this.loggedIn$ = this.store.select(state => state.uiReducer.loggedIn);
-    this.format$ = this.store.select(state => state.uiReducer.format);
+    this.apaFormat$ = this.store.select(state => state.uiReducer.format.apa);
+    this.chicagoFormat$ = this.store.select(state => state.uiReducer.format.chicago);
+    this.mlaFormat$ = this.store.select(state => state.uiReducer.format.mla);
+    
+    // Keep local format value up to date with store and resync on change
+    this.apaFormat$.subscribe((format) => {
+      if (format) {
+        this.format = 'apa';
+        this.syncReferences();
+      }
+    });
+    this.chicagoFormat$.subscribe((format) => {
+      if (format) {
+        this.format = 'chicago';
+        this.syncReferences();
+      }
+    });
+    this.mlaFormat$.subscribe((format) => {
+      if (format) {
+        this.format = 'mla';
+        this.syncReferences();
+      }
+    });
+
     // Sync references if loggedIn ever changes to True
     this.loggedIn$.subscribe((isLoggedIn) => {
-        if (isLoggedIn) this.store.dispatch({ type: 'SYNC', payload: this.format$ });
+      if (isLoggedIn) {
+        this.isLoggedIn = true;
+        this.syncReferences();
+      } else {
+        this.isLoggedIn = false;
+      }
     });
     // Check if a valid session cookie exists; if so, set state to loggedIn
     this.store.dispatch({ type: 'CHECK' });
