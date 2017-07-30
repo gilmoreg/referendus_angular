91e58a2c4347cdc67733d2220bc99f42c732c885
Grayson Gilmore <gilmoreg@live.com>
2017-07-30 17:48:31 -0600

    Add form validation for new article

--- client/src/app/modals/ref-modal/article/article.component.ts
+++ client/src/app/modals/ref-modal/article/article.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.css']
+  styleUrls: ['./article.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ArticleComponent implements OnInit {
+  defaultAuthor = 'Silva, Leonardo, Humberto';
+  defaultJournal = 'Journal of Software: Evolution and Process';
+  defaultTitle = 'Identifying Classes in Legacy JavaScript Code';
+  defaultUrl = 'http://dx.doi.org/10.1002/smr.1864';
+  defaultTags = 'JavaScript, OOP';
+  defaultNotes = '';
+  defaultYear = '2017';
+  defaultPages = 'n/a';
+  defaultVolume = 'n/a';
+  defaultIssue = 'n/a';
 
-  constructor() { }
+  constructor(private ref: ChangeDetectorRef) { }
 
   ngOnInit() {
+  }
+
+  formChanged() {
+    console.log('changed');
+    this.ref.detectChanges();
+  }
+
+  onSubmit(form) {
+    if (form.invalid) {
+      console.log('invalid', form);
+      return;
+    }
+    console.log(form.value);
   }
 
 }
