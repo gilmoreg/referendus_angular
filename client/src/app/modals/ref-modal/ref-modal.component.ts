2ef3b1cb8f51ee050ca2e4947337ac527c2a07d8
Grayson Gilmore <gilmoreg@live.com>
2017-07-29 18:41:04 -0600

    Force detect changes in new modal

--- client/src/app/modals/ref-modal/ref-modal.component.ts
+++ client/src/app/modals/ref-modal/ref-modal.component.ts
@@ -1,21 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, ChangeDetectionStrategy } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap/modal/modal-options.class';
 
 @Component({
   selector: 'app-ref-modal',
   templateUrl: './ref-modal.component.html',
   styleUrls: ['./ref-modal.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RefModalComponent implements OnInit {
-  type: string = 'article';
+  type: string;
   
-  constructor(public bsModalRef: BsModalRef) { }
+  constructor(public bsModalRef: BsModalRef, private ref: ChangeDetectorRef) { }
 
   ngOnInit() {
+    this.type = 'book';
   }
 
   changeType(type) {
     this.type = type;
+    this.ref.detectChanges();
   }
 
 }
