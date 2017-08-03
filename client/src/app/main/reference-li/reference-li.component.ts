91da5a8450a0c9ed3ab048f33241193b7b0d7d93
Grayson Gilmore <gilmoreg@live.com>
2017-08-03 10:19:55 -0600

    Add skeleton delete confirmation modal

--- client/src/app/main/reference-li/reference-li.component.ts
+++ client/src/app/main/reference-li/reference-li.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, TemplateRef } from '@angular/core';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/modal-options.class';
 
 @Component({
   selector: 'app-reference-li',
@@ -7,10 +9,15 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ReferenceLiComponent implements OnInit {
   @Input() reference;
+  public modalRef: BsModalRef;
 
-  constructor() { }
+  constructor(private modalService: BsModalService) { }
 
   ngOnInit() {
+  }
+
+  public openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
   }
 
 }
