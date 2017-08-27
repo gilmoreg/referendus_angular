df2db599c80e08d32a065eba6659605ac4514a18
Grayson Gilmore <gilmoreg@live.com>
2017-08-27 02:13:17 -0600

    Temp fix for heroku docker outage

--- heroku.sh
+++
@@ -1,7 +0,0 @@
-npm install -g --quiet @angular/cli@latest
-cd client
-npm install --quiet
-ng build --prod
-cp src/bootswatch.min.css src/client/node_modules/bootstrap/dist/css/bootswatch.min.css
-cd ~
-node server/server.js
